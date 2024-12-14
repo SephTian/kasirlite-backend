4c68a0711cdc41206d4ff15dded0c3408296ac05
JoJoseph <sephtianyt@gmail.com>
2024-12-14 17:41:06 +0800

    Feat: menu route

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import authRoute from './authRoute.js';
+import menuRoute from './menuRoute.js';
+import orderRoute from './orderRoute.js';
 
 // INITIATE ROUTE
 const router = Router();
 
 // GROUPING ROUTE THEN EXPORT IT TO APP.JS
 router.use('/auth', authRoute);
+router.use('/menu', menuRoute);
+router.use('/order', orderRoute);
 
 export default router;
