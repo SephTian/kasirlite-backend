c840f1e453a03e5bdcb63babc58770161380b148
JoJoseph <sephtianyt@gmail.com>
2025-01-23 20:47:11 +0800

    Feat: function to decode token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,10 @@
-import jsonwebtoken from 'jsonwebtoken';
+import jsonwebtoken, { verify, JwtPayload } from 'jsonwebtoken';
 const { sign } = jsonwebtoken;
 
 export function generateToken(id: number, email: string) {
   return sign({ id, email }, process.env.SECRET_KEY as string, { expiresIn: '7d' });
+}
+
+export function decodeToken(token: string) {
+  return verify(token, process.env.SECRET_KEY as string) as JwtPayload;
 }
